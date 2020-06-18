64de4d61d802257afb3746da05fae5a3768111ef
Andreas Lazar <alazar@allgeier.ch>
2020-06-18 10:44:18 +0200

    clean up 2

--- AFUT.Functions/ExampleFunction.cs
+++ AFUT.Functions/ExampleFunction.cs
@@ -26,9 +26,6 @@ namespace AFUT.Functions
             name = name ?? data?.name;
 
             string responseMessage = Helpers.GetResponseMessage(name);
-            // string.IsNullOrEmpty(name)
-            //     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //     : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
