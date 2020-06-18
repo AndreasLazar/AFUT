65037565000e9643aa317be03a152c07c4293f52
Andreas Lazar <alazar@allgeier.ch>
2020-06-18 10:28:31 +0200

    XUnit tests added

--- AFUT.Functions.Tests/UnitTest1.cs
+++ AFUT.Functions.Tests/UnitTest1.cs
@@ -1,14 +1,33 @@
 using System;
 using Xunit;
+using AFUT.Functions;
 
 namespace AFUT.Functions.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void EnsureNamePassedWorks()
         {
+            string name = "XUnit";
+            string response = Helpers.GetResponseMessage(name);
+            Assert.Equal($"Hello {name}", response);
+        }
 
+        [Fact]
+        public void EnsureNullPassedWorks()
+        {
+            string name = null;
+            string response = Helpers.GetResponseMessage(name);
+            Assert.Equal($"Please provide a name", response);
+        }
+        
+        [Fact]
+        public void EnsureEmptyStringPassedWorks()
+        {
+            string name = string.Empty;
+            string response = Helpers.GetResponseMessage(name);
+            Assert.Equal($"Please provide a name", response);
         }
     }
 }
